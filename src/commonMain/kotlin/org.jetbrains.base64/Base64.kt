dab95ed64da29a17126119bb344c267a22cc9f28
karthis@sterlingsoftware.co.in <karthis@sterlingsoftware.co.in>
2022-02-15 15:21:59 +0530

    First Commit

---
+++ src/commonMain/kotlin/org.jetbrains.base64/Base64.kt
@@ -0,0 +1,21 @@
+package org.jetbrains.base64
+
+/*interface Base64Encoder {
+    fun encode(src: ByteArray): ByteArray
+}*/
+
+interface Base64Encoder {
+    fun encode(src: ByteArray): ByteArray
+
+    fun encodeToString(src: ByteArray): String {
+        val encoded = encode(src)
+        return buildString(encoded.size) {
+            encoded.forEach { append(it.toInt().toChar()) }
+        }
+    }
+}
+
+
+expect object Base64Factory {
+    fun createEncoder(): Base64Encoder
+}
